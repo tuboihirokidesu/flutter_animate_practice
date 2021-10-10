d24ee0a2c439c74f13128c89c15b12fcd7710ba0
tuboihirokidesu <tuboi.hiroki1108@gmail.com>
2021-10-10 13:18:59 +0900

    コメント削除

--- lib/main.dart
+++ lib/main.dart
@@ -5,13 +5,6 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
-//コメント追加
-//コメント追加
-//コメント追加
-//コメント追加
-//コメント追加
-
-
 void main() => runApp(
     const MaterialApp(debugShowCheckedModeBanner: false, home: MyHomePage()));
 
