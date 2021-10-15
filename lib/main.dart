f73d447153584d16fd0ed2702022aad275006466
tuboihirokidesu <tuboi.hiroki1108@gmail.com>
2021-10-15 17:49:22 +0900

    コメント二つ追加

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,8 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
+//コメント追加
+//コメント追加
 void main() => runApp(
     const MaterialApp(debugShowCheckedModeBanner: false, home: MyHomePage()));
 
